32057cee740292042c199d722de72579d48dc10d
Shadowbear <lenniesi@web.de>
2025-08-25 15:56:11 +0200

    remove categoryName

--- src/Transaction.ts
+++ src/Transaction.ts
@@ -1,13 +1,8 @@
-import { type Account } from "./Account";
-import { type Category } from "./Category";
-import { type User } from "./User";
-
 export interface Transaction {
   id: string;
   userId: string;
   accountId: string;
   categoryId: string;
-  categoryName: string;
   date: string; // ISO string
   name: string;
   type: "income" | "expense";
